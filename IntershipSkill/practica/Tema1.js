465904e901923fa29ec3beca3b31d25fe26b36a6
b.tanase <b.tanase@gmail.com>
2023-12-24 19:51:19 +0200

    Ex.1

---
+++ IntershipSkill/practica/Tema1.js
@@ -0,0 +1,46 @@
+// 1.Creează o funcție care returnează valoarea 10
+
+const valoare = () => {
+  return 10;
+};
+
+console.log(valoare());
+
+//2.Creează o funcție care are o variabila definita local ‘numeleMeu’ și returnează numărul ei de caractere
+
+//3.Creează o funcție care are 2 variabile definite local a și b și returnează suma lor
+
+//4.Sa se creeze o functie care primeste trei parametrii ( d, e și f ) si returneaza produsul lor
+
+//5.Sa se creeze o functie care primeste un parametru ( x ) si returneaza diferența lui cu 10.
+//De exemplu dacă x = 16, funcția sa returneze 6
+
+//6.Sa se creeze o functie care primeste un parametru ( a ) si returneaza opusul lui.
+//De exemplu dacă a este 7 funcția sa returneze -7.
+
+//7.Sa se creeze o functie care primeste doi parametrii ( x si y ) si returneaza impartirea lor
+
+//8.Sa se creeze o functie care primeste un parametru ( notaMea ) si returneaza “Promovat” dacă
+//valoarea parametrului este peste 5 ( inclusiv ), în caz contrar returnează “Picat”
+
+//9.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+// diferența lor ( a - b ) este mai mare decât 10, atunci sa returneze true, altfel false.
+
+//10.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+//dacă cele doua numere sunt egale sa se returneze true, altfel false.
+
+//11.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+//returnează maximul. De exemplu, dacă a = 7, b = 5, funcția va returna 5
+
+//12.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și returnează minimul
+
+//13.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+//dacă diferența lor ( a - b ) este mai mare decât 10, atunci sa returneze true, altfel false.
+
+//14.Sa se creeze o funcție care are un parametru ( numeleMeu ) reprezentând un string.
+//Funcția sa afișeze în consola “Numele meu are “ + numărul de caractere al numelui + “ caractere “.
+
+//15.Sa se creeze o functie care are 2 parametrii ( numeleMeu, varstaMea ) reprezentand 2
+//un string și un număr. Functia sa afiseze in consola un string construit din cele 2
+//parametrii ca în exemplu: Dacă funcția este apelată cu argumentele “Andrei” și 30,
+//funcția sa afișeze în consola “Numele meu este Andrei și am 30 de ani”.
