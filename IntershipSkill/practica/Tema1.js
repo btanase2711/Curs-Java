39c9a88335332695ad7c26a4c08900f57dceef66
b.tanase <b.tanase@gmail.com>
2023-12-25 12:15:48 +0200

    Am terminat tema

--- IntershipSkill/practica/Tema1.js
+++ IntershipSkill/practica/Tema1.js
@@ -1,4 +1,4 @@
-// 1.Creează o funcție care returnează valoarea 10
+/* 1.Creează o funcție care returnează valoarea 10 */
 
 const valoare = () => {
   return 10;
@@ -6,7 +6,7 @@ const valoare = () => {
 
 console.log(valoare());
 
-//2.Creează o funcție care are o variabila definita local ‘numeleMeu’ și returnează numărul ei de caractere
+/* 2.Creează o funcție care are o variabila definita local ‘numeleMeu’ și returnează numărul ei de caractere */
 
 const numarCaractere = () => {
   const numeleMeu = "Bogdanovici";
@@ -14,7 +14,7 @@ const numarCaractere = () => {
 };
 console.log(numarCaractere());
 
-//3.Creează o funcție care are 2 variabile definite local a și b și returnează suma lor
+/* 3.Creează o funcție care are 2 variabile definite local a și b și returnează suma lor */
 
 const sumaNumerelor = () => {
   const a = 6;
@@ -24,7 +24,7 @@ const sumaNumerelor = () => {
 
 console.log(sumaNumerelor());
 
-//4.Sa se creeze o functie care primeste trei parametrii ( d, e și f ) si returneaza produsul lor
+/* 4.Sa se creeze o functie care primeste trei parametrii ( d, e și f ) si returneaza produsul lor */
 
 const produsulNumerelor = (d, e, f) => {
   return d * e * f;
@@ -32,8 +32,8 @@ const produsulNumerelor = (d, e, f) => {
 
 console.log(produsulNumerelor(3, 4, 5));
 
-//5.Sa se creeze o functie care primeste un parametru ( x ) si returneaza diferența lui cu 10.
-//De exemplu dacă x = 16, funcția sa returneze 6
+/* 5.Sa se creeze o functie care primeste un parametru ( x ) si returneaza diferența lui cu 10.
+De exemplu dacă x = 16, funcția sa returneze 6 */
 
 const difernetaX = (x) => {
   return x - 10;
@@ -41,23 +41,23 @@ const difernetaX = (x) => {
 
 console.log(difernetaX(8));
 
-//6.Sa se creeze o functie care primeste un parametru ( a ) si returneaza opusul lui.
-//De exemplu dacă a este 7 funcția sa returneze -7.
+/* 6.Sa se creeze o functie care primeste un parametru ( a ) si returneaza opusul lui.
+De exemplu dacă a este 7 funcția sa returneze -7. */
 
 const opusul = (a) => {
   return -1 * a;
 };
 console.log(opusul(145));
 
-//7.Sa se creeze o functie care primeste doi parametrii ( x si y ) si returneaza impartirea lor
+/* 7.Sa se creeze o functie care primeste doi parametrii ( x si y ) si returneaza impartirea lor */
 
 const impartire = (x, y) => {
   return x / y;
 };
 console.log(impartire(15, 5));
 
-//8.Sa se creeze o functie care primeste un parametru ( notaMea ) si returneaza “Promovat” dacă
-//valoarea parametrului este peste 5 ( inclusiv ), în caz contrar returnează “Picat”
+/* 8.Sa se creeze o functie care primeste un parametru ( notaMea ) si returneaza “Promovat” dacă
+valoarea parametrului este peste 5 ( inclusiv ), în caz contrar returnează “Picat” */
 
 const verificarePromovare = (notaMea) => {
   let status;
@@ -71,8 +71,8 @@ const verificarePromovare = (notaMea) => {
 
 console.log(verificarePromovare(5));
 
-//9.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
-// daca diferența lor ( a - b ) este mai mare decât 10, atunci sa returneze true, altfel false.
+/* 9.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+daca diferența lor ( a - b ) este mai mare decât 10, atunci sa returneze true, altfel false. */
 
 const statusDiferenta = (a, b) => {
   let status;
@@ -82,21 +82,61 @@ const statusDiferenta = (a, b) => {
 
 console.log(statusDiferenta(10, 9));
 
-//10.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
-//dacă cele doua numere sunt egale sa se returneze true, altfel false.
+/* 10.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+dacă cele doua numere sunt egale sa se returneze true, altfel false. */
 
-//11.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
-//returnează maximul. De exemplu, dacă a = 7, b = 5, funcția va returna 5
+const checkEgal = (a, b) => {
+  return a === b ? "true" : "false";
+};
 
-//12.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și returnează minimul
+console.log(checkEgal("5", "5"));
 
-//13.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
-//dacă diferența lor ( a - b ) este mai mare decât 10, atunci sa returneze true, altfel false.
+/* 11.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+returnează maximul. De exemplu, dacă a = 7, b = 5, funcția va returna 5 */
 
-//14.Sa se creeze o funcție care are un parametru ( numeleMeu ) reprezentând un string.
-//Funcția sa afișeze în consola “Numele meu are “ + numărul de caractere al numelui + “ caractere “.
+const maxim = (a, b) => {
+  return a > b ? a : b;
+};
 
-//15.Sa se creeze o functie care are 2 parametrii ( numeleMeu, varstaMea ) reprezentand 2
-//un string și un număr. Functia sa afiseze in consola un string construit din cele 2
-//parametrii ca în exemplu: Dacă funcția este apelată cu argumentele “Andrei” și 30,
-//funcția sa afișeze în consola “Numele meu este Andrei și am 30 de ani”.
+console.log(maxim(8, 72));
+
+/* 12.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și returnează minimul */
+
+const minim = (a, b) => {
+  return a < b ? a : b;
+};
+
+console.log(minim(4, 5));
+
+/* 13.Sa se creeze o funcție care primeste doi parametrii ( a si b ) și
+dacă diferența lor ( a - b ) este mai mare decât 10, atunci sa returneze true, altfel false. */
+
+const checkDiff = (a, b) => {
+  return a - b > 10 ? "true" : "false";
+};
+
+console.log(checkDiff(17, 2));
+
+/* 14.Sa se creeze o funcție care are un parametru ( numeleMeu ) reprezentând un string.
+Funcția sa afișeze în consola “Numele meu are “ + numărul de caractere al numelui + “ caractere “. */
+
+const numarCaract = (numeleMeu) => {
+  /*   let numarCar = " ";
+  numarCar = numeleMeu.length; */
+  return "Numele meu are " + numeleMeu.length + " caractere";
+};
+
+console.log(numarCaract("Alex"));
+
+/* 15.Sa se creeze o functie care are 2 parametrii ( numeleMeu, varstaMea ) reprezentand 2
+un string și un număr. Functia sa afiseze in consola un string construit din cele 2
+parametrii ca în exemplu: Dacă funcția este apelată cu argumentele “Andrei” și 30,
+funcția sa afișeze în consola “Numele meu este Andrei și am 30 de ani”. */
+
+const introducere = (numeleMeu, varstaMea) => {
+  //   return "Numele meu este " + numeleMeu + " si am " + varstaMea + " de ani";
+  return `Numele meu este ${numeleMeu} si am ${varstaMea} de ani`;
+};
+console.log(introducere("Mihai", 29));
+
+// trebuia sa declar in prealabil variabilele ca fiind string si numar, sa nu apara erori ?
